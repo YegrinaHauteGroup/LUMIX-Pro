b290319d567689a5da613654cbb7b2d95a5502f2
Yegrina Haute Group <official.yegrina@gmail.com>
2026-06-24 22:41:25 +0900

    Update DashboardCharts.tsx

--- components/features/dashboard/DashboardCharts.tsx
+++ components/features/dashboard/DashboardCharts.tsx
@@ -22,7 +22,7 @@ interface ChartProps {
 
 export function DashboardCharts({ genderStats, statusStats, classStats }: ChartProps) {
   return (
-    <>
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       <Card>
         <CardHeader><CardTitle>성별 현황</CardTitle></CardHeader>
         <CardContent>
